ab3b1daaba81a63840a8ac1d573f2340b7f8dd94
Dmitry <kirillovdmitrii_at@mail.ru>
2022-11-06 21:50:41 +0500

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+#ShareHome
+
+ShareHome is telegram-bot for animal shelter which helps to take a dog or cat to home
+
+##Team
+Developing by **NinjaTeam**
+- Elena
+- Dmitry
+- Aleksandr
---
+++ src/test/java/com/skypro/telegrambot/sharehome/SharehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.telegrambot.sharehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharehomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
